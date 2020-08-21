1dfb7bfd8d3f1574d3f69691484c50ca73651dce
Sebastian Szuber <sebastian.m.szuber@gsk.com>
2020-08-21 13:58:03 +0200

    Updated system_setup

--- vmsetup/system_setup.sh
+++ vmsetup/system_setup.sh
@@ -8,3 +8,20 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
 apt-get update -y && \
     export ACCEPT_EULA=Y && \
     apt-get install msodbcsql17 mssql-tools unixodbc-dev -y
+cat > /etc/odbc.ini <<EOF
+# [DSN name]
+[sqlserver01pyapp]
+Driver = ODBC Driver 17 for SQL Server
+# Server = [protocol:]server[,port]
+Server = tcp:sqlserver01pyapp.database.windows.net,1433
+Database = notejamdb01
+#
+# Note:
+# Port is not a valid keyword in the odbc.ini file
+# for the Microsoft ODBC driver on Linux or macOS
+#
+EOF
+adduser njuser --home /home/njuser --shell /bin/bash --disabled-password
+echo 'njuser       ALL=(ALL:ALL) ALL' >> /etc/sudoers
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /home/njuser/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /home/njuser/.bashrc
