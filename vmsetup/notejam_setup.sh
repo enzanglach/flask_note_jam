66892391c4f86a397ef4826d242b3607b3bfffff
Sebastian Szuber <sebastian.m.szuber@gsk.com>
2020-09-01 14:02:25 +0200

    Updated config

--- vmsetup/notejam_setup.sh
+++ vmsetup/notejam_setup.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
-apt-get update -y
-apt-get install -y curl gnupg
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
-apt-get update -y && \
-    export ACCEPT_EULA=Y && \
-    apt-get install msodbcsql17 mssql-tools unixodbc-dev -y
-
-
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-export PATH="$PATH:/opt/mssql-tools/bin"
-
-odbc.ini /etc/odbc.ini
-
- . /flaskapp
- /flaskapp
- /usr/local/bin/python -m pip install --upgrade pip
- pip install --no-cache-dir -r requirements.txt
-
-# Run the command on container startup
-CMD export FLASK_APP=notejam && \
-    export FLASK_ENV=development && \
-    flask run --host 0.0.0.0 --port=80
+cd /home/njuser
+git clone -b vmimage --single-branch https://github.com/enzanglach/flask_note_jam.git
+cd /home/njuser/flask_note_jam
+pip3 install virtualenv
+~/.local/bin/virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
