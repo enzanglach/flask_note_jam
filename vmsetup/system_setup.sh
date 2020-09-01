759ff0a0278d0383dd42ed8c7e91bbc0504c729b
Sebastian Szuber <sebastian.m.szuber@gsk.com>
2020-09-01 14:02:39 +0200

    Updated config

--- vmsetup/system_setup.sh
+++ vmsetup/system_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update -y
-apt-get install -y curl gnupg
+apt-get install -y curl gnupg git apache2 libapache2-mod-wsgi python3-dev python3-pip
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
@@ -25,3 +25,16 @@ adduser njuser --home /home/njuser --shell /bin/bash --disabled-password
 echo 'njuser       ALL=(ALL:ALL) ALL' >> /etc/sudoers
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /home/njuser/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /home/njuser/.bashrc
+cd /home/azureuser
+GITHUB_PAT=`cat github_personal_access_token`
+echo "export GITHUB_PAT=$GITHUB_PAT" >> /home/njuser/.bash_profile
+echo "export GITHUB_PAT=$GITHUB_PAT" >> /home/njuser/.bashrc
+SQLALCHEMY_DATABASE_URI=`cat sqlalchemy_database_uri`
+echo "export SQLALCHEMY_DATABASE_URI=$SQLALCHEMY_DATABASE_URI" >> /home/njuser/.bash_profile
+echo "export SQLALCHEMY_DATABASE_URI=$SQLALCHEMY_DATABASE_URI" >> /home/njuser/.bashrc
+echo 'export FLASK_APP=notejam' >> /home/njuser/.bash_profile
+echo 'export FLASK_APP=notejam' >> /home/njuser/.bashrc
+echo 'export FLASK_ENV=production' >> /home/njuser/.bash_profile
+echo 'export FLASK_ENV=production' >> /home/njuser/.bashrc
+chown njuser:njuser /home/njuser/.bash_profile
+a2enmod wsgi
